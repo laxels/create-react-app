9d1eecc6686551d182d75f600920333f70d8ed7b
Axel Setyanto <laxels@gmail.com>
2018-08-08 11:10:20 -0700

    Remove Coffee, enable decorators and .babelrc

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -167,7 +167,7 @@ module.exports = {
             loader: require.resolve('babel-loader'),
             options: {
               // @remove-on-eject-begin
-              babelrc: false,
+              babelrc: true,
               presets: [require.resolve('babel-preset-react-app')],
               // @remove-on-eject-end
               // This is a feature of `babel-loader` for webpack (not Babel itself).
